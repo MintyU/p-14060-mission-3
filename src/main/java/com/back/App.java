8415e1c48e616a1a53a90114f354ecb8ad156a5b
MintyU <mintyu.dev@gmail.com>
2025-11-06 10:33:25 +0900

    [Implement] First Commit

---
+++ src/main/java/com/back/App.java
@@ -0,0 +1,35 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (cmd.startsWith("등록")) {
+
+            } else if (cmd.startsWith("목록")) {
+
+            } else if (cmd.startsWith("수정")) {
+
+            } else if (cmd.startsWith("삭제")) {
+
+            } else {
+                System.out.println("알 수 없는 명령입니다.");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -0,0 +1,5 @@
+package com.back.domain.wiseSaying.entity;
+
+public class WiseSaying {
+
+}
