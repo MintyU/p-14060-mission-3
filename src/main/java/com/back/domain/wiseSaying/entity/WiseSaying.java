f8671c9fd78504015dc07fb6a3ed0a43e6f18f74
MintyU <mintyu.dev@gmail.com>
2025-11-06 11:23:00 +0900

    [Implement] WiseSaying entity implement

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -1,5 +1,35 @@
 package com.back.domain.wiseSaying.entity;
 
 public class WiseSaying {
+    private final int id;
+    private String quote;
+    private String author;
+
+    public WiseSaying(int id, String quote, String author) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
 
 }
