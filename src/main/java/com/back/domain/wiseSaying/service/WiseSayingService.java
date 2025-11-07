1dd25b8859247965ee84b5f0fef11191c303433e
MintyU <mintyu.dev@gmail.com>
2025-11-07 11:47:24 +0900

    [ãImplement] WiseSayingService implement

---
+++ src/main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
@@ -0,0 +1,38 @@
+package com.back.domain.wiseSaying.service;
+
+import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.domain.wiseSaying.repository.JsonRepository;
+
+public class WiseSayingService {
+    private final JsonRepository repo = new JsonRepository("db/wiseSaying");
+
+    // 명언 추가
+    public void add(String quote, String author) {
+        int id = repo.nextId();
+        WiseSaying ws = new WiseSaying(id, quote, author);
+        repo.save(ws);
+        repo.flush("db/wiseSaying");
+    }
+    // 명언 수정 (존재하는 경우 수정하고 true 반환, 없으면 false 반환)
+    public boolean update(int id, String quote, String author) {
+        var optionalWiseSaying = repo.findById(id);
+        if (optionalWiseSaying.isEmpty()) {
+            return false;
+        }
+        WiseSaying ws = optionalWiseSaying.get();
+        ws.setQuote(quote);
+        ws.setAuthor(author);
+        repo.save(ws);
+        repo.flush("db/wiseSaying");
+        return true;
+    }
+
+    // 명언 삭제 (존재하는 경우 삭제하고 true 반환, 없으면 false 반환)
+    public boolean delete(int id) {
+        boolean deleted = repo.deleteById(id);
+        if (deleted) {
+            repo.flush("db/wiseSaying");
+        }
+        return deleted;
+    }
+}
